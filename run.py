15caad56ce43946e77019d60e71bdf4eed05139e
dineshsreekanthan <dinsree11@gmail.com>
2023-09-28 22:03:37 +0530

    updated program list

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ status = "pass"
 
 project_folder =  py.config.var["project_folder"]
 
-program_list = [project_folder + '/py/0_health_bot.py', project_folder + '/py/1_main_playwright.py', project_folder + '/py/2_process_excel.py', project_folder + '/py/3_send_email_outlook.py']
+program_list = [project_folder + '/py/0.py', project_folder + '/py/1.py', project_folder + '/py/2.py', project_folder + '/py/3.py']
 
 for program in program_list:
     subprocess.run(['python', program])
